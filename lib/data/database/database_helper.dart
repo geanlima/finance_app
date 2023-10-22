28a7c58d47d2194a8788765c4cbdd625b70cbf07
Gean da Silva Lima <gean.lima@multicomnet.com.br>
2023-10-22 09:31:08 -0300

    totalizado

--- lib/data/database/database_helper.dart
+++ lib/data/database/database_helper.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: depend_on_referenced_packages
+// ignore_for_file: depend_on_referenced_packages, unused_local_variable
 
 import 'package:finance_app/models/group.dart';
 import 'package:finance_app/models/expense.dart';
